5190b441b8d97351ae095eed4f752fd9189a6ce9
Robin <rajnish.robin@gmail.com>
2018-11-26 13:00:12 +0530

    Delete TwitterSourceConstant.java

--- src/main/java/kafka/TwitterSourceConstant.java
+++
@@ -1,8 +0,0 @@
-package kafka;
-
-public class TwitterSourceConstant {
-public static final String CONSUMER_KEY_KEY = "GOZNNC0b9zBm4sgv0JR370fqT";
-public static final String CONSUMER_SECRET_KEY = "AcHw1OAq04arkpGq0msH3q4giIeqNLedxtL3uAKtxgl4DhqXJ8";
-public static final String ACCESS_TOKEN_KEY = "268249306-qMuqxMxPhxR7HHorGBSuuNkjvAS7fgdcdll1Fzw2";
-public static final String ACCESS_TOKEN_SECRET_KEY = "ekd0uD5GzNwCOPy6ysAwm8NGaZPbPoHKKrM1fiCmGH84g";
-}
