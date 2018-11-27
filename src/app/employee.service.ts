376e6e30fa93e66ab12d2b756fcfef5078cdb491
surajrocks1997 <surajrocks1997@gmail.com>
2018-11-27 10:58:09 +0530

    added json file

---
+++ src/app/employee.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IEmployee } from './employee';
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private _url: string = "assets/data/employee.json";
+  
+
+  constructor(private http: HttpClient) { }
+  
+  getEmployees(): Observable<IEmployee[]>{
+    return this.http.get<IEmployee[]>(this._url);
+  }
+}
