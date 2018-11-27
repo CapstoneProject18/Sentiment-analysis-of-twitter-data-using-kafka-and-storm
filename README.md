74235b0416a63ab19935531ab02096f1c6f21faf
Pujam Janghel <pujamjanghel@gmail.com>
2018-11-27 18:06:26 +0530

    some commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sentiment-analysis-of-twitter-data-using-kafka-and-storm
+The idea here is to build a data pipeline between apache kafka and storm to do sentiment analysis
---
+++ storm-pro/src/main/java/twitter/storm/kafka/App.java
@@ -0,0 +1,13 @@
+package twitter.storm.kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
