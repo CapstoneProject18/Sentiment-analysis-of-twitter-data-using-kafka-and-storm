6fbded8ce121f43aff90abd6922318846ab73d9a
BeBinaries <rajnish.robin@gmail.com>
2018-12-21 16:34:36 +0530

    new updates

--- spring-boot-rest/src/main/java/com/fayaz/rest/controller/controller.java
+++ spring-boot-rest/src/main/java/com/fayaz/rest/controller/controller.java
@@ -1,15 +1,16 @@
-package com.json.str;
+package com.fayaz.rest.controller;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class controller {
 	
@@ -17,7 +18,7 @@ public class controller {
 	 
 	@RequestMapping("/json-api")
 	public String GetAllMovies() throws IOException {
-		 File file = new File("/home/robin/tweets-only.json"); 
+		 File file = new File("/home/robin/myfile.txt"); 
 		  
 		 
 		try {
---
+++ spring-boot-rest/src/main/java/com/fayaz/server/main/SpringBootRestApplication.java
@@ -0,0 +1,14 @@
+package com.fayaz.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.fayaz.rest.controller"})
+public class SpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApplication.class, args);
+	}
+
+}
+
